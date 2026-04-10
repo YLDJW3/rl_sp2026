b70b01446a4f7051edbe58c5b79982e74dcd04c3
youngcr <974227915@qq.com>
2026-04-11 00:58:28 +0800

    feat: flow matching policy

--- hw1/src/hw1_imitation/model.py
+++ hw1/src/hw1_imitation/model.py
@@ -61,6 +61,7 @@ class MSEPolicy(BasePolicy):
         state: torch.Tensor,
         action_chunk: torch.Tensor,
     ) -> torch.Tensor:
+        # In MSE policy, training has the same forward process as inference
         y = self.sample_actions(state)
         d = y - action_chunk
         return d.square().mean()
@@ -72,18 +73,14 @@ class MSEPolicy(BasePolicy):
         num_steps: int = 10,
     ) -> torch.Tensor:
         assert state.shape[-1] == self.state_dim    # (... state_dim)
-        y = self.forward(state)  # (... chunk_size * action_dim)
+        y = self.layers(state)  # (... chunk_size * action_dim)
         assert y.shape[-1] == self.chunk_size * self.action_dim
         return y.unflatten(dim=-1, sizes=(self.chunk_size, self.action_dim))
-    
-    def forward(self, state: torch.Tensor) -> torch.Tensor:
-        return self.layers(state)
 
 
 class FlowMatchingPolicy(BasePolicy):
     """Predicts action chunks with a flow matching loss."""
 
-    ### TODO: IMPLEMENT FlowMatchingPolicy HERE ###
     def __init__(
         self,
         state_dim: int,
@@ -91,14 +88,35 @@ class FlowMatchingPolicy(BasePolicy):
         chunk_size: int,
         hidden_dims: tuple[int, ...] = (128, 128),
     ) -> None:
+        assert len(hidden_dims) >= 2
         super().__init__(state_dim, action_dim, chunk_size)
+        self.hidden_dims = hidden_dims
+        # input: (state, \tau)
+        layers = [nn.Linear(state_dim + chunk_size * action_dim + 1, hidden_dims[0]), nn.ReLU()]
+        for x, y in zip(hidden_dims[:-1], hidden_dims[1:]):
+            layers.append(nn.Linear(x, y))
+            layers.append(nn.ReLU())
+        # output layer: predict chunk_size actions
+        layers.append(nn.Linear(hidden_dims[-1], chunk_size * action_dim)) 
+        self.layers = nn.Sequential(*layers)
+        self.layers.apply(init_rl_weights)
 
     def compute_loss(
         self,
-        state: torch.Tensor,
-        action_chunk: torch.Tensor,
+        state: torch.Tensor,    # (batch_size, state_dim)
+        action_chunk: torch.Tensor, # (batch_size, chunk_size, action_dim)
     ) -> torch.Tensor:
-        raise NotImplementedError
+        action_chunk = action_chunk.flatten(-2, -1)
+        # `randn` normal distribution
+        action_init = torch.randn_like(action_chunk)    # (batch_size, chunk_size * action_dim)
+        # `rand` uniform distribution
+        t = torch.rand(state.shape[:-1]).unsqueeze(-1) # (batch_size, 1)
+        action_t = action_chunk * t + action_init * (1 - t)
+        x = torch.cat([state, action_t, t], dim=-1)
+        y = self.forward(x)
+        target = action_chunk - action_init
+        d = y - target
+        return d.square().mean()
 
     def sample_actions(
         self,
@@ -106,7 +124,18 @@ class FlowMatchingPolicy(BasePolicy):
         *,
         num_steps: int = 10,
     ) -> torch.Tensor:
-        raise NotImplementedError
+        action_t = torch.randn((*state.shape[:-1], self.chunk_size * self.action_dim))  # (batch_size, chunk_size*action_dim)
+        for i in range(num_steps):
+            t = torch.full(state.shape[:-1], i / num_steps).unsqueeze(-1)   # (batch_size, 1)
+            x = torch.cat([state, action_t, t], dim=-1) # (batch_size, state_dim+chunk_size*action_dim+1)
+            y = self.forward(x)
+            action_t += 1 / num_steps * y
+        return action_t.unflatten(-1, (self.chunk_size, self.action_dim))
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # (... state_dim+chunk_size*action_dim+1)
+        assert x.shape[-1] == self.state_dim + self.chunk_size * self.action_dim + 1   
+        return self.layers(x)  # (... chunk_size * action_dim)
 
 
 PolicyType: TypeAlias = Literal["mse", "flow"]
--- note/hw1.md
+++ note/hw1.md
@@ -6,6 +6,17 @@ batch_size = 128
 data_size = 180
 num_epochs = 400
 
+# MSE policy
+1. Use a multi-layer perceptron (MLP) network with two hidden layers, use ReLU as activation function, and use Adam optimizer with learning rate 1e-3
+2. Training and inference share the same process
+
+# Flow matching policy
+1. Use a multi-layer perceptron (MLP) network with two hidden layers, use ReLU as activation function, and use Adam optimizer with learning rate 1e-3
+2. Training and inference have different processes
+    During training, sample time steps **uniformly** from [0, 1]
+    During inference, given number of steps N, calculate time steps as $t_i = \frac{i}{N}$ for $i=0, 1, ..., N-1$  
+
+
 # Result
 1. https://wandb.ai/yangzf23-independent-developer/hw1-imitation/runs/ht9h3adi?nw=nwuseryangzf23
 2. `reward=0.65` at `step=70_000`
